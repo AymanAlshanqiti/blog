be21edfe291d16f091561bfae4be347e252acff8
Ayman <ayman.alshanqiti@gmail.com>
2021-05-09 05:52:13 +0300

    add articles list & article form

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const articleRouter = require('./routes/articles');
 
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-	res.render('');
+	const articles = [
+		{
+			title: 'title 1',
+			description: 'description of title 1',
+			markdown: 'description of title `1`',
+			createdAt: new Date(),
+		},
+		{
+			title: 'title 2',
+			description: 'description of title 2',
+			markdown: 'description of title `2`',
+			createdAt: new Date(),
+		},
+		{
+			title: 'title 3',
+			description: 'description of title 3',
+			markdown: 'description of title `3`',
+			createdAt: new Date(),
+		},
+	];
+	res.render('articles/index', { articles });
 });
+
+app.use('/articles', articleRouter);
 
 app.listen(5000);
